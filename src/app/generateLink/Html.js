50b6c25d7b8fb2e806b3e193bd0a69dfd54cbf69
Frontend RVRenterCenter <frontend@rvrentercenter.com>
2024-03-14 15:21:17 +0500

    chnages

--- src/app/generateLink/Html.js
+++ src/app/generateLink/Html.js
@@ -91,14 +91,14 @@ const Html = () => {
                 <div className='sidebar-left-content'>
 
 
-                    <div class="card">
+                    <div className="card">
                         <div className='card-header'>
                             <div className='main_title_head d-flex justify-content-between align-items-center'>
-                                <h3 class="link_default m-0"><i class="fa fa-bullhorn link_icon" aria-hidden="true"></i> Default Links
+                                <h3 className="link_default m-0"><i className="fa fa-bullhorn link_icon" aria-hidden="true"></i> Default Links
                                 </h3>
 
                                 <div className=''>
-                                    <div class="d-flex align-items-center gap-3">
+                                    <div className="d-flex align-items-center gap-3">
                                         <div className="position-relative">
                                             <button className="btn btn-primary btn-sm " onClick={()=>setSelectDropdown(!SelectDropdown)}>
                                                 Dynamic Parameters <i className='fa fa-angle-down'></i>
@@ -118,7 +118,7 @@ const Html = () => {
                                                         {isChecked[checkbox.key] && (
                                                             <div className="">
                                                                 <input type="text" className="inpudesgn mr-2" name={checkbox.key} value={parameters[checkbox.key]} onChange={handleInputChange} placeholder={`Enter ${checkbox.label} Value`} />
-                                                                <i className="fa fa-times class-bx" aria-hidden="true"></i>
+                                                               
                                                             </div>
                                                           
                                                         )}
@@ -126,7 +126,7 @@ const Html = () => {
                                                     </div>
                                                 ))}
                                                <div className='text-end'>
-                                               <button className="btn btn-primary mb-3" onClick={handleSubmit}>Submit</button>
+                                               <button className="btn btn-primary btn-sm mb-3" onClick={handleSubmit}>Submit</button>
                                                </div>
                                             </div>}
                                         </div>
@@ -143,15 +143,15 @@ const Html = () => {
 
                         </div>
                         <div className='card-body'>
-                            <h2 class="fiver_cpa">Fiverr CPA</h2>
-                            <div class="input-group mb-2">
+                            <h5 className="fiver_cpa mb-3">Fiverr CPA</h5>
+                            <div className="input-group mb-2">
 
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
+                                <div className="input-group-prepend">
+                                    <div className="input-group-text">
                                         <i className="fa fa-clipboard copy_icon" aria-hidden="true" onClick={copyText}></i>
                                     </div>
                                 </div>
-                                <p id="textToCopy" class="form-control mb-0" >{url || 'https://upfilly.jcsoftwaresolution.in/'}</p>
+                                <p id="textToCopy" className="form-control br0 mb-0" >{url || 'https://upfilly.jcsoftwaresolution.in/'}</p>
                                 
                             </div>
                            
