98941ac6e29f4d39febe4a7349b8ccca2ab0e91b
Your Name <admin>
2024-03-14 14:22:23 +0530

    link design

--- src/app/generateLink/Html.js
+++ src/app/generateLink/Html.js
@@ -1,8 +1,73 @@
 import datepipeModel from '@/models/datepipemodel';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/global/layout';
+import ApiClient from '@/methods/api/apiClient';
+import loader from '@/methods/loader';
 
 const Html = () => {
+    const [isChecked, setIsChecked] = useState({});
+    const [parameters, setParameters] = useState({
+      product: '',
+      affiliate_id: '',
+      XYZ: '',
+      New_id: ''
+    });
+    const[url,setUrl]=useState('')
+  
+    const checkboxValues = [
+      { key: "product", label: "Product" },
+      { key: "affiliate_id", label: "Affiliate ID" },
+      { key: "XYZ", label: "XYZ" },
+      { key: "New_id", label: "New ID" }
+    ];
+  
+    const handleCheckboxChange = (key) => {
+      setIsChecked(prevState => ({
+        ...prevState,
+        [key]: !prevState[key]
+      }));
+    };
+  
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setParameters(prevParams => ({
+        ...prevParams,
+        [name]: value
+      }));
+    };
+
+    // useEffect(()=>{
+    //     ApiClient.get('get-link').then((res) => {
+    //         if (res?.success) {
+    //             setUrl(res?.data)
+    //             console.log(res?.data, "=======resssss")
+    //         }
+    //     loader(false)
+    //     })
+    // },[])
+  
+    const handleSubmit = () => {
+        const checkedParameters = {};
+        Object.keys(isChecked).forEach(key => {
+            if (isChecked[key]) {
+                checkedParameters[key] = parameters[key];
+            }
+        });
+        loader(true)
+        ApiClient.post('get-link', {"base_url":"https://upfilly.jcsoftwaresolution.in/", "parameters": checkedParameters }).then((res) => {
+            if (res?.success) {
+                setUrl(res?.data)
+                console.log(res?.data, "=======resssss")
+            }
+        loader(false)
+        })
+
+        const queryParams = Object.keys(checkedParameters)
+            .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(checkedParameters[key]))
+            .join('&');
+        const url = 'http://example.com?' + queryParams;
+        console.log(url, "urlurl")
+    };
 
     return (
         <>
@@ -18,72 +83,37 @@ const Html = () => {
 
                                 <div className=''>
                                     <div class="d-flex align-items-center gap-3">
-
-                                    <div class="dropdown">
-                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            DYNAMIC PARAMETERS
-                                        </button>
-                                        <div class="dropdown-menu width_menu" aria-labelledby="dropdownMenuButton">
-                                            <div class="d-flex align-items-center gap-3 justify-content-between px-2 border-bottom">
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="gridCheck"/>
-                                                        <label class="form-check-label" for="gridCheck">
-                                                            Tracking Code
-                                                        </label>
-                                                </div>
-                                                <div class="d-flex gap-3 align-items-center">
-                                                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
-                                                        <i class="fa fa-times class-bx" aria-hidden="true"></i>
-
-                                                </div>
+                                        <div className="dropdown">
+                                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                DYNAMIC PARAMETERS
+                                            </button>
+                                            <div className="dropdown-menu width_menu" aria-labelledby="dropdownMenuButton">
+                                                {checkboxValues.map((checkbox, index) => (
+                                                    <div key={index} className="d-flex align-items-center gap-3 justify-content-between px-2 border-bottom">
+                                                        <div className="form-check">
+                                                            <input className="form-check-input" type="checkbox" id={`gridCheck${index}`} checked={isChecked[checkbox.key]} onChange={() => handleCheckboxChange(checkbox.key)} />
+                                                            <label className="form-check-label" htmlFor={`gridCheck${index}`}>
+                                                                {checkbox.label}
+                                                            </label>
+                                                        </div>
+                                                        {isChecked[checkbox.key] && (
+                                                            <div className="d-flex gap-3 align-items-center">
+                                                                <input type="text" className="form-control" name={checkbox.key} value={parameters[checkbox.key]} onChange={handleInputChange} placeholder={`Enter ${checkbox.label} Value`} />
+                                                                <i className="fa fa-times class-bx" aria-hidden="true"></i>
+                                                            </div>
+                                                        )}
+                                                    </div>
+                                                ))}
+                                                <button className="btn btn-primary mt-3" onClick={handleSubmit}>Submit</button>
                                             </div>
-                                            <p class=""></p>
-                                            <ul>
-                                                <li>    <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="gridCheck"/>
-                                                        <label class="form-check-label" for="gridCheck">
-                                                            Tracking Code
-                                                        </label>
-                                                </div></li>
-                                                <li>    <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="gridCheck"/>
-                                                        <label class="form-check-label" for="gridCheck">
-                                                            Tracking Code
-                                                        </label>
-                                                </div></li>
-
-                                                <li>    <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="gridCheck"/>
-                                                        <label class="form-check-label" for="gridCheck">
-                                                            Tracking Code
-                                                        </label>
-                                                </div></li>
-
-                                                <li>    <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="gridCheck"/>
-                                                        <label class="form-check-label" for="gridCheck">
-                                                            Tracking Code
-                                                        </label>
-                                                </div></li>  <li>    <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="gridCheck"/>
-                                                        <label class="form-check-label" for="gridCheck">
-                                                            Tracking Code
-                                                        </label>
-                                                </div></li>
-                                            </ul>
-
-
                                         </div>
-                                    </div>
-
-
-                                    <div className='select_one'>
+                                    {/* <div className='select_one'>
                                         <select className='selectwidth '>
                                             <option>One</option>
                                             <option>One</option>
                                             <option>One</option>
                                         </select>
-                                    </div>
+                                    </div> */}
                                 </div>
                                 </div>
                         </div>
@@ -97,7 +127,7 @@ const Html = () => {
                                 <div class="input-group-text"><i class="fa fa-clipboard copy_icon" aria-hidden="true"></i>
                                 </div>
                             </div>
-                            <p class="form-control mb-0" ></p>
+                            <p class="form-control mb-0" >{url || 'https://upfilly.jcsoftwaresolution.in/'}</p>
                         </div>
                     </div>
                    
