1914f4e657642cad4da22c7a1fcad3ae9ea16855
Frontend RVRenterCenter <frontend@rvrentercenter.com>
2024-07-29 18:33:49 +0530

    changes in script

---
+++ public/fprmain.js
@@ -0,0 +1,4 @@
+// /public/fprmain.js
+(function(w){w.fpr=w.fpr||function(){w.fpr.q = w.fpr.q||[];w.fpr.q[arguments[0]=='set'?'unshift':'push'](arguments);};})(window);
+fpr("init", {cid:"bry6ko3r"}); 
+fpr("click");
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'react-toastify/dist/ReactToastify.css';
 import "./scss/main.scss";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,27 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <head>
-      <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w){
-                w.fpr=w.fpr||function(){
-                  w.fpr.q = w.fpr.q||[];
-                  w.fpr.q[arguments[0]=='set'?'unshift':'push'](arguments);
-                };
-              })(window);
-              fpr("init", {cid:"bry6ko3r"});
-              fpr("click");
-              `
-            }}
-          />
-          {/* Add the external script */}
-          <script
-            src="https://cdn.firstpromoter.com/fpr.js"
-            async
-          />
-      </head>
+      <Head>
+        <script src="fprmain.js"/>
+        <script src="https://cdn.firstpromoter.com/fpr.js" />
+      </Head>
       <body className={inter.className}>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
         <GoogleOAuthProvider clientId="817638365394-413bppqmd901dl9u6ml6na7an7f02r9s.apps.googleusercontent.com">
