6c7d4b811cf6600fa22c868b265b1c07313d1b2c
Frontend RVRenterCenter <frontend@rvrentercenter.com>
2024-09-17 19:07:37 +0530

    remove console and css pull

--- src/app/affiliate-form/taxSection/[id]/page.js
+++ src/app/affiliate-form/taxSection/[id]/page.js
@@ -473,8 +473,8 @@ const Publish = () => {
                               social_security_number: e.target.value,
                             });
                             // console.log(
-                              form?.social_security_number.replaceAll("-", "")
-                            );
+                            //   form?.social_security_number.replaceAll("-", "")
+                            // );
                           }}
                         />
                         {sumitted && !form?.social_security_number && (
