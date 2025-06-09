86027d5a5d7f0b679a69164dd12a5b8a094eab4a
akshay <akshaysharma@jcsoftwaresolution.com>
2025-06-09 18:33:49 +0530

    add data

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -11,7 +11,6 @@ import crendentialModel from "../../models/credential.model";
 import { requestForToken } from '../firebase/function';
 import FacebookLogin from '@greatsumini/react-facebook-login';
 import axios from 'axios';
-import { toast } from 'react-toastify';
 
 export default function Login() {
   const history = useRouter()
